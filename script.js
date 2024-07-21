5ab5dce9c8ac78f892f422628fcf3d461fd135fc
Mohammad Ashoori <mohammadashoori.87@gmail.com>
2024-07-21 08:06:24 -0700

    [Create classes object]
    Create classes object and add student

--- script.js
+++ script.js
@@ -1,13 +1,76 @@
 //======================
 //=====Function Show Log
-function log_console(){
+function log_console() {
 
     //=====Clear Console Log :
     console.clear();
 
     //=====School Object Variable :
     let school = {
+        
+        //=====Class A Object Variable :
+        classA: {
 
+            //=====Student 1 Object Variable :
+            student1: {
+                name: "Mohammad",
+                family: "Ashoori",
+                phone: "+98 993 958 4110",
+                address: "Bousher",
+                score: 20
+            },
+
+            //=====Student 2 Object Variable :
+            student2: {
+                name: "Mohammad",
+                family: "Ashoori",
+                phone: "+98 993 958 4110",
+                address: "Bousher",
+                score: 20
+            },
+
+            //=====Student 3 Object Variable :
+            student3: {
+                name: "Mohammad",
+                family: "Ashoori",
+                phone: "+98 993 958 4110",
+                address: "Bousher",
+                score: 20
+            }
+
+        },
+
+        //=====Class B Object Variable :
+        classB: {
+
+            //=====Student 1 Object Variable :
+            student1: {
+                name: "Mohammad",
+                family: "Ashoori",
+                phone: "+98 993 958 4110",
+                address: "Bousher",
+                score: 20
+            },
+
+            //=====Student 2 Object Variable :
+            student2: {
+                name: "Mohammad",
+                family: "Ashoori",
+                phone: "+98 993 958 4110",
+                address: "Bousher",
+                score: 20
+            },
+
+            //=====Student 3 Object Variable :
+            student3: {
+                name: "Mohammad",
+                family: "Ashoori",
+                phone: "+98 993 958 4110",
+                address: "Bousher",
+                score: 20
+            }
+
+        }
     }
 
 }
