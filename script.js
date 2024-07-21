d09639be290399b940d6b16c9a09d1c706d0601e
Mohammad Ashoori <mohammadashoori.87@gmail.com>
2024-07-21 08:15:22 -0700

    [Show data console log]
    Show data student for console log in script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function log_console() {
 
     //=====School Object Variable :
     let school = {
-        
+
         //=====Class A Object Variable :
         classA: {
 
@@ -72,5 +72,49 @@ function log_console() {
 
         }
     }
+
+    //=====Show Data School In Console Log
+    console.log("============================================")
+    console.log("data student 1 in class a : ")
+    console.log("Name : ", school.classA.student1.name);
+    console.log("Family : ", school.classA.student1.family);
+    console.log("Phone : ", school.classA.student1.phone);
+    console.log("Address : ", school.classA.student1.address);
+    console.log("Score : ", school.classA.student1.score);
+    console.log("======================")
+    console.log("data student 2 in class a : ")
+    console.log("Name : ", school.classA.student2.name);
+    console.log("Family : ", school.classA.student2.family);
+    console.log("Phone : ", school.classA.student2.phone);
+    console.log("Address : ", school.classA.student2.address);
+    console.log("Score : ", school.classA.student2.score);
+    console.log("======================")
+    console.log("data student 3 in class a : ")
+    console.log("Name : ", school.classA.student3.name);
+    console.log("Family : ", school.classA.student3.family);
+    console.log("Phone : ", school.classA.student3.phone);
+    console.log("Address : ", school.classA.student3.address);
+    console.log("Score : ", school.classA.student3.score);
+    console.log("============================================")
+    console.log("data student 1 in class b : ")
+    console.log("Name : ", school.classB.student1.name);
+    console.log("Family : ", school.classB.student1.family);
+    console.log("Phone : ", school.classB.student1.phone);
+    console.log("Address : ", school.classB.student1.address);
+    console.log("Score : ", school.classB.student1.score);
+    console.log("======================")
+    console.log("data student 2 in class b : ")
+    console.log("Name : ", school.classB.student2.name);
+    console.log("Family : ", school.classB.student2.family);
+    console.log("Phone : ", school.classB.student2.phone);
+    console.log("Address : ", school.classB.student2.address);
+    console.log("Score : ", school.classB.student2.score);
+    console.log("======================")
+    console.log("data student 3 in class b : ")
+    console.log("Name : ", school.classB.student3.name);
+    console.log("Family : ", school.classB.student3.family);
+    console.log("Phone : ", school.classB.student3.phone);
+    console.log("Address : ", school.classB.student3.address);
+    console.log("Score : ", school.classB.student3.score);
 
 }
